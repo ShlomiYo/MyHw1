b14416c1ea0831727936b430fca561d129bdf996
ShlomiYo <brezrk1@gmail.com>
2015-06-04 15:57:39 +0300

    DFA

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ var port = process.env.PORT || 8080;
 
 
 app.use(express.static("publicDir"));
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get("/oldestMember", function(req, res){
 
