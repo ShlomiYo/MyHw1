2ac7308d430c32cd8aac98822fd5278bf53acb68
ShlomiYo <brezrk1@gmail.com>
2015-06-04 15:15:00 +0300

    SD

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ app.get("/bestSeller", function(req, res){
 
     res.setHeader('Content-Type', 'application/json');
 	app.set("json spaces", 1);
+	app.set('json replacer', replacer);
 	res.json(msg);
 });
 
